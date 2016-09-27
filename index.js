4fef7d13dea297116c73d9fcb6fa14599762defb
Kristi Thompson <kt.devcat@gmail.com>
2016-09-27 14:01:07 -0400

    maybe this will work

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild(){
+  return document.querySelector(#grand-node div div div div)
+}
+
+function increaseRankBy(n){
+  var list = document.querySelectorAll('ul.ranked-list li')
+  for (i = 0; i < list.length; i++){
+    list[i] = list[i] + 1;
+  }
+}
